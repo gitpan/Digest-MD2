928528ded8c70b5fc52c952a9f0db4c2d5d863e1
Gisle Aas <gisle@ActiveState.com>
2003-07-23 06:35:15 +0000

    Import of GAAS/Digest-MD2-2.02 from CPAN.
    gitpan-cpan-distribution: Digest-MD2
    gitpan-cpan-version:      2.02
    gitpan-cpan-path:         GAAS/home/gisle/p/md2/Digest-MD2-202targz/Digest-MD2-2.02.tar.gz
    gitpan-cpan-author:       GAAS
    gitpan-cpan-maturity:     released

--- MD2.pm
+++ MD2.pm
@@ -3,7 +3,7 @@ package Digest::MD2;
 use strict;
 use vars qw($VERSION @ISA @EXPORT_OK);
 
-$VERSION = '2.01';  # $Date: 2002/12/27 09:42:19 $
+$VERSION = '2.02';  # $Date: 2003/07/23 06:33:38 $
 
 require Exporter;
 *import = \&Exporter::import;
@@ -62,7 +62,7 @@ L<Digest::MD5>
 This library is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
 
- Copyright 1998-2001 Gisle Aas.
+ Copyright 1998-2003 Gisle Aas.
  Copyright 1990-1992 RSA Data Security, Inc.
 
 =head1 AUTHOR
